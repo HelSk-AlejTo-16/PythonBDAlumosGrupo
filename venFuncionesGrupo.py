8b19fd502b1f5626e59845180d0fe2bef394297e
AplicacionAndroidCRUD <leyotorres1501@gmail.com>
2026-03-11 20:32:36 -0600

    Creación de Limpiar, Eliminar y buscar

--- venFuncionesGrupo.py
+++ venFuncionesGrupo.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient as MC
 
 ventana = tk.Tk()
 ventana.title("Agregar Grupo")
-ventana.geometry("400x200")
+ventana.geometry("800x400")
 ventana.resizable(0,0)
 ventana.config(cursor="hand2")
 
@@ -83,6 +83,94 @@ btn_Agregar.grid(row=3, column=0)
 # Botón Actualizar
 btn_Actualizar = tk.Button(ventana, text="Actualizar", font=("Arial", 12, "bold"), bg="white", fg="black", command=Actualizar)
 btn_Actualizar.grid(row=3, column=1)
+
+
+#Función para ver grupo
+def VerUnGrupo():
+    # .get().strip() elimina espacios accidentales al inicio o final
+    clave = ent_cveGru.get().strip()
+    nombre = ent_nomGru.get().strip()
+
+    if clave and nombre:
+        Grupo_Existente = Grupos.find_one({"cveGru": clave, "nomGru": nombre})
+        
+        if not Grupo_Existente:
+            messagebox.showerror("Error", "No existe un grupo con esos datos.")
+            ent_cveGru.delete(0, tk.END)
+            ent_nomGru.delete(0, tk.END)
+        else:
+          
+            resumen = f"Clave: {Grupo_Existente['cveGru']}\nNombre: {Grupo_Existente['nomGru']}"
+            messagebox.showinfo("Éxito", resumen)
+    elif clave or nombre:
+        messagebox.showwarning("Atención", "Por favor llene ambos campos.")
+    else:
+      
+        lista_grupos = Grupos.find()
+        listado = "--Listado de todos los grupos --\n \n"
+
+        for grupo in lista_grupos:
+            listado += f"{grupo['cveGru']} - {grupo['nomGru']}\n"
+        messagebox.showinfo("Todos los grupos", listado)
+#botón buscar.
+btn_buscarGrupo = tk.Button(ventana, text="Buscar",font=("Arial",12, "bold"), bg="white", fg="black", command=VerUnGrupo)
+btn_buscarGrupo.grid(row=1, column=2 , padx=10, pady=5)
+
+#Limpiar Grupo.
+def Limpiar():
+    clave = ent_cveGru.get()
+    nombre = ent_nomGru.get()
+    if not clave and not nombre:
+       messagebox.showerror("Atención", "No hay nada para borrar")
+
+    else:
+        
+        ent_nomGru.delete(0,tk.END)
+        ent_cveGru.delete(0,tk.END)
+
+
+btn_limpiar = tk.Button(ventana, text="Limpiar", font=("Arial",12, "bold"), bg="white", fg="black", command=Limpiar)
+btn_limpiar.grid(row=2, column=2, padx=10, pady=5)
+
+
+def EliminarGrupo(): 
+    clave = ent_cveGru.get().strip()
+    nombre = ent_nomGru.get().strip()
+
+    if clave and nombre:
+        Grupo_Existente = Grupos.find_one({"cveGru": clave, "nomGru": nombre})
+
+        if not Grupo_Existente:
+            messagebox.showerror("Error", "No existe un grupo con esos datos.")
+            ent_cveGru.delete(0, tk.END)
+            ent_nomGru.delete(0, tk.END)
+        
+        else:
+            Grupos.delete_one({"cveGru":clave, "nomGru":nombre})
+            messagebox.showinfo("Éxito", "Se borró exitosamente el grupo")
+            ##Falta borrar los alumnos :p
+    elif clave or nombre:
+            messagebox.showerror("Atención", "Por favor llene ambos campos.")
+
+    else:
+        messagebox.showerror("Error", "Llene los dos apartados.")
+
+btn_borrarGrupo= tk.Button(ventana, text="Eliminar", font=("Arial",12,"bold"), bg="white", fg="black", command=EliminarGrupo)
+btn_borrarGrupo.grid(row=3, column=2, padx=10, pady=5)
+
+#Exportar csv
+
+
+btn_exportarCsv = tk.Button(ventana, text="Exportar csv", font=("Arial",12, "bold"), bg="white", fg="black")
+btn_exportarCsv.grid(row=4, column=0,padx=10, pady=5 )
+#Exportar json
+btn_exportarJson = tk.Button(ventana, text="Exportar json", font=("Arial",12, "bold"), bg="white", fg="black")
+btn_exportarJson.grid(row=4, column=1,padx=10, pady=5 )
+#Exportar xml
+
+btn_exportarXml = tk.Button(ventana, text="Exportar xml", font=("Arial",12, "bold"), bg="white", fg="black")
+btn_exportarXml.grid(row=4, column=2,padx=10, pady=5 )
+
 
 
 ventana.mainloop()
